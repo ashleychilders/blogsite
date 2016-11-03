577d6789d79b17758bdc38a8ea35d6c97b4a205b
Ashley Childers <ashley.childers@gmail.com>
2016-11-03 15:21:53 -0400

    Update to header image

--- header.php
+++ header.php
@@ -24,9 +24,9 @@
     </div>
     <?php if ( get_header_image() ) : ?>
     <div class="site-header">
-      <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
             <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-        </a></h1>
+        </a>
     </div>
     </div>
 <?php endif; ?>
