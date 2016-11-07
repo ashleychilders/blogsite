474627d01dbc0281e53420c82513cfe5519a9bec
Ashley Childers <ashley.childers@gmail.com>
2016-11-06 22:25:51 -0500

    Clean up code

--- header.php
+++ header.php
@@ -13,35 +13,34 @@
   <script src-"custom.js"></script>
 -->
 <body>
-<div class="header-wrapper">
-  <header class="container">
-    <div class="row">
-        <div class="tweleve columns">
-          <div class="logotop">
-            <?php dynamic_sidebar('header-one'); ?>
-          </div>
-        </div>
-    </div>
-    <?php if ( get_header_image() ) : ?>
-    <div class="site-header">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-        </a>
-    </div>
-    </div>
-<?php endif; ?>
+  <div class="header-wrapper">
+    <header class="container">
       <div class="row">
           <div class="tweleve columns">
-            <div class="blank-menu-header">
-            <?php
-              wp_nav_menu(array(
-                  'sort_column' => 'menu_order',
-                  'container_class' => 'blank-menu-header'
-              ));
-              ?>
+            <div class="logotop">
+              <?php dynamic_sidebar('header-one'); ?>
             </div>
           </div>
+      </div>
+        <?php if ( get_header_image() ) : ?>
+      <div class="site-header">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+              <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+          </a>
+      </div>
+      </div>
+        <?php endif; ?>
+        <div class="row">
+            <div class="tweleve columns">
+              <div class="blank-menu-header">
+              <?php
+                wp_nav_menu(array(
+                    'sort_column' => 'menu_order',
+                    'container_class' => 'blank-menu-header'
+                ));
+                ?>
+              </div>
+            </div>
           </div>
-      </header>
-  </div>
+        </header>
     <div class="container">
