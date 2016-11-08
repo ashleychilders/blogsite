6ee5084166b5d0be30fac05dc4e873a3d8b251e9
Ashley Childers <ashley.childers@gmail.com>
2016-11-07 20:19:21 -0500

    Update margin on about and search

--- search.php
+++ search.php
@@ -3,25 +3,24 @@
 get_header(); ?>
 
 <div class="row">
-    <div class="eight columns">
+  <div class="searchpage">
+    <div class="tweleve columns">
         <?php if( have_posts() ) :?>
-            <h1><?php printf(
+            <h2><?php printf(
                   __('Search Results for: %s'),
                   '<span>' . get_search_query() . '</span>' );
-                ?></h1>
+                ?></h2>
             <?php
                 while (have_posts()) : the_post(); ?>
-                    <h2><?php the_title(); ?></h2>
+                    <h3><?php the_title(); ?></h3>
                     <?php the_content();
                 endwhile;
           else : ?>
-            <h1>Nothing Found</h1>
+            <h3>Nothing Found</h3>
             <p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
         <?php endif; ?>
     </div>
-    <div class="four columns">
-        <?php get_sidebar(); ?>
-    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
